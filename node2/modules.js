5e5b4f2c87a0980293af28c56716eef8990859ec
chaemin <w2114@e-mirim.hs.kr>
2023-03-21 18:54:28 +0900

    방과후 3일차

---
+++ node2/modules.js
@@ -0,0 +1,11 @@
+//절대값 구하는 메서드
+exports.abs = function(number){
+  if(0<number){
+    return number;
+  }else{
+    return -number;
+  }
+};
+exports.circleArea = function (radius){
+  return radius * radius * Math.PI;
+}
